384098bb37591d3760823b848b445e8509d6301f
hamzagilani8 <65051395+hamzagilani8@users.noreply.github.com>
2023-03-11 11:05:05 +0500

    typo

--- components/education.tsx
+++ components/education.tsx
@@ -7,31 +7,16 @@ const Education = ({ data }: { data: any }) => {
         <div className="text-black border-l-4 border-yellow pl-10 pb-2 pr-10 m-10 ">
             <h1 className='text-4xl'>{title}</h1>
             <ul className={`flex flex-wrap justify-around content-center py-4 gap-5`}>
-                {study.map(({
-                    educationLevel,
-                    institute,
-                    degree,
-                    duration,
-                    grades,
-                    latest
-                }: {
-                    educationLevel: string,
-                    institute: string,
-                    degree: string,
-                    duration: string,
-                    grades: string
-                    latest:boolean
-                }) => (
-                    <div className={`${latest ? "border-l-green" : "border-l-grey"} border-l-4 p-4`}>
+                {study.map(({educationLevel,institute,degree,duration,grades,latest}: {educationLevel: string, institute: string,degree: string, duration: string,grades: string, latest:boolean}) => (
+                        <li key={educationLevel} className={`${latest ? "border-l-green" : "border-l-grey"} border-l-4 p-4`}>
                         <h2 className="text-base text-black-light">{educationLevel}</h2>
                         <h1 className={`${latest ? "text-green" : "text-yellow-dark"} text-2xl  font-extrabold`}>{degree}</h1>
                         <h3 className="text-lg text-black-light">{grades}</h3>
                         <h1 className="text-xl ">{institute}</h1>
                         <p className="text-sm">{duration}</p>
-                        </div>
+                        </li>
                 ))}
             </ul>
-
         </div>
 
     )
